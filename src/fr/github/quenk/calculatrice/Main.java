ce2aa4bb2a29ff8d887fbe47c98ac5379e783956
BuildTools <unconfigured@null.spigotmc.org>
2022-05-05 11:39:47 +0200

    Calculatrice cmd

--- src/fr/github/quenk/calculatrice/Main.java
+++ src/fr/github/quenk/calculatrice/Main.java
@@ -7,18 +7,38 @@ public class Main {
     public static void main(String[] args) {
         int nombreUn;
         int nombreDeux;
-        int resulat;
+        String operateur;
+        int resulat = 0;
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Bienvenue sur la petite calculatrice de QuenK.");
         System.out.println("Veuillez entrer votre premier nombre:");
         nombreUn = scanner.nextInt();
+        System.out.println("Veuillez entrer votre opérateur:");
+        operateur = String.valueOf(scanner.next().charAt(0));
         System.out.println("Veuillez entrer votre deuxième nombre:");
         nombreDeux = scanner.nextInt();
+        switch(operateur){
+            case "*":
+                resulat = nombreUn * nombreDeux;
+                break;
+            case "-":
+                resulat = nombreUn - nombreDeux;
+                break;
+            case "+":
+                resulat = nombreUn + nombreDeux;
+                break;
+            case "/":
+                resulat = nombreUn / nombreDeux;
+                break;
+            default :
+                System.out.println("Opérateur non trouvé.");
+                scanner.close();
+                break;
 
-        resulat = nombreUn+nombreDeux;
-        System.out.println("Voici votre résultat: "+resulat);
-
+        }
+        System.out.println("résultat:" +resulat);
+        scanner.close();
     }
 }
